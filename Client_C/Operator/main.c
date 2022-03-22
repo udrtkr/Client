fa52abd3396328147dbd511c60bd0664c9422585
udrtkr <zxcv008816@gmail.com>
2022-03-22 19:48:33 +0900

    ob

--- Client_C/Operator/main.c
+++ Client_C/Operator/main.c
@@ -14,7 +14,7 @@ int main(void) {
 	// -
 	// *
 	// /
-	// %
+	// % 정수연산만 가능
 
 	// 부호 연산자
 	// + - 붙여
@@ -39,6 +39,44 @@ int main(void) {
 	// 피연산자 두 개 따져 true = 1 false = 0
 
 	// 비트 연산자
-	// & | ~
+	// & |  ^(xor) 다르면1같으면0  ~
+	
+	// 비트 shift
+	// << overflow bit 사라짐
+	// >> (5 >> 3)
+
+	// 삼항연산자 조건 ? 참일때 결과 : 거짓일때 결과
+	
+	// ,(콤마 연산자)
+	//----------------------------
+	int commaResult = 0;
+	int a = 0, b = 0;
+	commaResult = a = 3, b = 4;
+
+	// sizeof()
+	// 메모리 크기를 바이트단위로 계산해서 반환
+	int size_int = sizeof(int);
+	int size_size_int = sizeof(size_int);
+	printf("%d %d \n", size_int, size_size_int);
+
+	// 형변환 연산자
+	// ----------------------------
+	// 암시적 자동으로 변환
+	int num1_int = 10;
+	double num_double = 12.32;
+	short num_short = 2;
+	char num_char = 4;
+	// 연산을 하기 위해 레지스터에 데이터 전달 시 int형 변환된 채로 연산하게 됨
+	// 
+	printf("%d \n", sizeof(num_short + num_char));
+
+	// 오버플로우에 의한 암시적 형변환
+
+	// 명시적
+	// 데이터 형 적어 변환
+	int n;
+	float f;
+	f = 5 / 3;
+	n = (int)(3.7 + 7.7);
 	
 }
